f14a57ea3fd6e72a8f1f21af0bcd3f916f8f7c9e
mcfurry <mcfurry.only@gmail.com>
2023-10-03 15:05:57 +0300

    initial commit

---
+++ src/test/java/pro/sky/teamproject/TeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.teamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
