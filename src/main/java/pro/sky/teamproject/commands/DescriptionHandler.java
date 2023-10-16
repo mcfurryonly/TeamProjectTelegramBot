bdbc4a9a1e064374a59be038f1af32cb7707e3e8
Дмитрий <dimakrjukov@gmail.com>
2023-10-16 17:11:53 +0200

    Не меняться callbackData

--- src/main/java/pro/sky/teamproject/commands/DescriptionHandler.java
+++ src/main/java/pro/sky/teamproject/commands/DescriptionHandler.java
@@ -3,8 +3,14 @@ package pro.sky.teamproject.commands;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
+@Component
 public class DescriptionHandler implements ExtendedHandler {
 
     private final String INFO = "/info";
@@ -19,41 +25,38 @@ public class DescriptionHandler implements ExtendedHandler {
 
     @Override
     public void handle(Update update, String userChoice) {
-        Message message = update.message();
-        if (message != null && message.text() != null) {
-            String text = message.text();
-            Long chatId = message.chat().id();
-            switch (text) {
-                case INFO:
-                    if (userChoice.equals("Dog")) {
-                        bot.execute(new SendMessage(chatId, "Информация о собаке"));
-                    } else if (userChoice.equals("Cat")) {
-                        bot.execute(new SendMessage(chatId, "Информация о кошке"));
-                    }
-                    break;
-                case TAKE:
-                    if (userChoice.equals("Dog")) {
-                        bot.execute(new SendMessage(chatId, "Вы берете собаку из приюта"));
-                    } else if (userChoice.equals("Cat")) {
-                        bot.execute(new SendMessage(chatId, "Вы берете кошку из приюта"));
-                    }
-                    break;
-                case REPORT:
-                    if (userChoice.equals("Dog")) {
-                        bot.execute(new SendMessage(chatId, "Отчет о собаке"));
-                    } else if (userChoice.equals("Cat")) {
-                        bot.execute(new SendMessage(chatId, "Отчет о кошке"));
-                    }
-                    break;
-                case VOLUNTEER:
-                    if (userChoice.equals("Dog")) {
-                        bot.execute(new SendMessage(chatId, "Вот номер волонтера для собак +7 *** ***"));
-                    } else if (userChoice.equals("Cat")) {
-                        bot.execute(new SendMessage(chatId, "Вот номер волонтера для кошек +7 *** ***"));
-                    }
-                    break;
+
+        InlineKeyboardButton infoButton = new InlineKeyboardButton("Инфо").callbackData(INFO);
+        InlineKeyboardButton takeButton = new InlineKeyboardButton("Взять").callbackData(TAKE);
+        InlineKeyboardButton reportButton = new InlineKeyboardButton("Отчет").callbackData(REPORT);
+        InlineKeyboardButton volunteerButton = new InlineKeyboardButton("Позвать волонтера").callbackData(VOLUNTEER);
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup(infoButton,takeButton,reportButton,volunteerButton);
+
+            long chatId = update.callbackQuery().message().chat().id();
+            SendMessage sendMessage = new SendMessage(chatId,
+                    "Выберите действие")
+                    .replyMarkup(markup);
+            bot.execute(sendMessage);
+        if (update.callbackQuery() != null) {
+            var callbackData = update.callbackQuery().data();
+
+            if (Objects.equals(userChoice, "Dog")){
+                assert callbackData != null;
+                if (callbackData.equals(INFO)){ //Каллбекнеться Dog нужно /info
+                    bot.execute(new SendMessage(chatId, "Тут будет инфо о собаке"));
+                }
+                // Добавьте здесь обработку других команд для "Dog"
+            } else if (Objects.equals(userChoice, "Cat")) {
+                assert callbackData != null;
+                if (callbackData.equals(INFO)){
+                    SendMessage sendMessage1 = new SendMessage(chatId, "Тут будет инфо о кошке");
+                    bot.execute(sendMessage1);
+                }
+                // Добавьте здесь обработку других команд для "Cat"
             }
         }
+
+
     }
 
     @Override
--- src/main/java/pro/sky/teamproject/commands/StartHandler.java
+++ src/main/java/pro/sky/teamproject/commands/StartHandler.java
@@ -6,6 +6,7 @@ import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -15,6 +16,8 @@ public class StartHandler implements Handler {
 
     private final String START = "/start";
     private final TelegramBot bot;
+
+    @Autowired
     private DescriptionHandler descriptionHandler;
 
     public StartHandler(TelegramBot bot) {
@@ -26,24 +29,34 @@ public class StartHandler implements Handler {
      */
     public void handle(Update update) {
         String DOG = "Dog";
-        InlineKeyboardButton dogButton = new InlineKeyboardButton("Приют собак").callbackData(DOG);
         String CAT = "Cat";
+        InlineKeyboardButton dogButton = new InlineKeyboardButton("Приют собак").callbackData(DOG);
         InlineKeyboardButton catButton = new InlineKeyboardButton("Приют кошек").callbackData(CAT);
         InlineKeyboardMarkup markup = new InlineKeyboardMarkup(dogButton, catButton);
-        SendMessage sendMessage = new SendMessage(update.message().chat().id(),
-                "Добро пожаловать в приложение")
-                .replyMarkup(markup);
-        bot.execute(sendMessage);
-        if (update.message().text().equals(DOG)) {
-            descriptionHandler.handle(update, DOG);
-        } else if (update.message().text().equals(CAT)) {
-            descriptionHandler.handle(update, CAT);
-        }else {
-            bot.execute(new SendMessage(update.message().chat().id(), "Что то пошло не так"));
+
+        if (update.message() != null) {
+            long chatId = update.message().chat().id();
+            SendMessage sendMessage = new SendMessage(chatId,
+                    "Добро пожаловать в приложение")
+                    .replyMarkup(markup);
+            bot.execute(sendMessage);
         }
 
-    }
+        if (update.callbackQuery() != null) {
+            var callbackData = update.callbackQuery().data();
+            long chatId = update.callbackQuery().message().chat().id();
 
+            if (callbackData.equals(DOG)) {
+                bot.execute(new SendMessage(chatId, "Собака"));
+                descriptionHandler.handle(update, DOG);
+            } else if (callbackData.equals(CAT)) {
+                bot.execute(new SendMessage(chatId, "Кот"));
+                descriptionHandler.handle(update, CAT);
+            } else {
+                bot.execute(new SendMessage(chatId, "Что то пошло не так"));
+            }
+        }
+    }
 
     @Override
     public boolean isSuitable(Update update) {
@@ -53,12 +66,4 @@ public class StartHandler implements Handler {
                 .map(el -> el.equals(START))
                 .orElse(false);
     }
-
-///    @Override
-///    public boolean isSuitable(Update update) {
-//        if (update != null && update.message() != null && update.message().text() != null) {
-//            return update.message().text().equals(START);
-//        }
-//        return false;
-//    }
 }
