4c45ca97db9ff0ccb35df01653b0510d7f6f1b7e
Дмитрий <dimakrjukov@gmail.com>
2023-10-18 01:25:22 +0200

    Buttons was reworked successfully 8 buttons work

--- src/main/java/pro/sky/teamproject/commands/ExtendedHandler.java
+++
@@ -1,9 +0,0 @@
-package pro.sky.teamproject.commands;
-
-import com.pengrad.telegrambot.model.Update;
-
-public interface ExtendedHandler {
-    void handle(Update update, String userChoice);
-
-    boolean isSuitable(Update update);
-}
